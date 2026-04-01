b3e6103caca431c4418532620fa554e3f67e4809
TakWolf <takwolf@foxmail.com>
2026-04-01 10:22:11 +0800

    小点阵

--- www/js/app.js
+++ www/js/app.js
@@ -5,8 +5,8 @@ createApp({
     data() {
         return {
             infos: db,
-            fontFamily: 'MingLiU',
-            fontSize: 16,
+            fontFamily: '–°',
+            fontSize:8,
             input: 'ABCDEFGHIJKLMNOPQRSTUVWXYZ\nabcdefghijklmnopqrstuvwxyz\n0123456789 !"#$%&\'()*+,-./:;<=>?@[\\]^_`{|}~￥\n\n我们度过的每个平凡的日常，也许就是连续发生的奇迹。',
         }
     },
